00cbcc76d79034cdfea122867c3a11226471bf7d
JohnQuimson <john.quimson777@gmail.com>
2023-11-23 01:24:03 +0100

    feat: aggiunta dom

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -1,16 +1,43 @@
 'use strict';
 
+/*
+<div class="container">
+      <img src="" alt="">
+      <h3></h3>
+      <span></span>
+    </div>
+*/
+
 const biciCorsa = [
   {
-    nome: race,
+    nome: 'race',
     peso: 8,
+    img: 'img/race.png',
   },
   {
-    nome: endurance,
+    nome: 'endurance',
     peso: 9,
+    img: 'img/endurance.png',
   },
   {
-    nome: race,
+    nome: 'aero',
     peso: 7,
+    img: 'img/aero.png',
   },
 ];
+
+const container = document.querySelector('.container');
+
+biciCorsa.forEach((biciCorsa, index) => {
+  const { nome, peso, img } = biciCorsa;
+
+  const bike = ` <div class="card">
+      <div class="cont-img">
+        <img src="${img}" alt="${index}" />
+      </div>
+      <h3>Nome: ${nome}</h3>
+      <span>peso: ${peso} kg</span>
+    </div>`;
+
+  container.innerHTML += bike;
+});
