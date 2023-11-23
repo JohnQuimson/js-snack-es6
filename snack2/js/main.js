65c116561819df27643b5c84c514197bebc2c4fe
JohnQuimson <john.quimson777@gmail.com>
2023-11-23 02:05:43 +0100

    feat: assegnazione values

--- snack2/js/main.js
+++ snack2/js/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const teams = [
+  {
+    nome: 'Lakers',
+    punti: undefined,
+    falli: undefined,
+  },
+  {
+    nome: 'Chicago',
+    punti: undefined,
+    falli: undefined,
+  },
+  {
+    nome: 'Warriors',
+    punti: undefined,
+    falli: undefined,
+  },
+];
+
+console.log(teams);
+
+teams.forEach((squadra) => {
+  squadra.punti = Math.floor(Math.random() * 80) + 1;
+  squadra.falli = Math.floor(Math.random() * 10) + 1;
+});
